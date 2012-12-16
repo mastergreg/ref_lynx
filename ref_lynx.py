d36cd41d2b2566463f9c8b0ad5eff960c8287d74
Gregory Liras <gregliras@gmail.com>
2012-12-17 00:34:24 +0200

    init

---
+++ ref_lynx.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8
+#
+#* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+# File Name : ref_lynx.py
+# Creation Date : 16-12-2012
+# Last Modified : Mon 17 Dec 2012 12:32:39 AM EET
+# Created By : Greg Liras <gregliras@gmail.com>
+#_._._._._._._._._._._._._._._._._._._._._.*/
+
+from sys import stdin
+import re
+from conf import api_key, username
+
+import bitly_api
+
+
+def main():
+    data = stdin.read()
+    r = re.compile(" *[1-9][0-9]*\. .*")
+    bcl = bitly_api.Connection(username, api_key)
+    print data[:data.rfind("References")]
+    print "References:"
+    for ans in re.findall(r, data):
+        ans = ans.split()
+        btlurl = bcl.shorten(ans[1])
+        print ans[0], btlurl['url']
+
+
+
+if __name__=="__main__":
+    main()
+
